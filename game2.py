54257fc9d73448bd64c9efdccbade8f1ebe632a1
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2018-05-17 06:11:21 +0800

    Initialized work for gamescreens

--- game2.py
+++ game2.py
@@ -3,7 +3,9 @@ from Utilities import *
 from Classes.Label import Label
 from Classes.Scoreboard import Scoreboard
 from Classes.Player import Player
-
+from Label import Label as joshLabel
+import Colors
+import Settings
 
 def Gameover(winner: str, text: str):
     sprites.empty()
@@ -40,74 +42,99 @@ def init():
     fps = 60
     done = False
     p1creating, p2creating = False, False
+    gameScreen = True
+    bgMusic = pygame.mixer.Sound("bg.ogg").play(-1)
+    gameTitle = joshLabel("MainFont.otf", "Trail Blazers", 50, Colors.YELLOW1,
+                          (Settings.SCREEN[0] // 2, (Settings.SCREEN[1] // 2)-120))
+    easy = joshLabel("MainFont.otf", "F1             Easy Mode", 20, Colors.YELLOW1,
+                          (Settings.SCREEN[0] // 2, (Settings.SCREEN[1] // 2)-35))
+    normal = joshLabel("MainFont.otf", "        F2            Normal Mode", 20, Colors.YELLOW1,
+                     (Settings.SCREEN[0] // 2, (Settings.SCREEN[1] // 2)+5))
+    hard = joshLabel("MainFont.otf", "F3            Hard Mode", 20, Colors.YELLOW1,
+                     (Settings.SCREEN[0] // 2, (Settings.SCREEN[1] // 2)+45))
+    about = joshLabel("MainFont.otf", "Game    Developers:      jjjimenez100     &&     jh-diaz", 12, Colors.YELLOW1,
+                     (Settings.SCREEN[0] // 2, Settings.SCREEN[1]-75))
+    images = joshLabel("MainFont.otf", "Image and Music Resources:  opengameart.org", 12, Colors.YELLOW1,
+                     (Settings.SCREEN[0] // 2, Settings.SCREEN[1]-55))
+    fonts = joshLabel("MainFont.otf", "Font   Family   Resources:     font1001.com", 12, Colors.YELLOW1,
+                       (Settings.SCREEN[0] // 2, Settings.SCREEN[1] - 35))
+    gameScreenLabels = pygame.sprite.Group(gameTitle, easy, normal, hard, about, images, fonts)
 
     while not done:
         clock.tick(fps)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    done = True
-                if event.key == pygame.K_LEFT:
-                    p2.changeDirection('left')
-                if event.key == pygame.K_RIGHT:
-                    p2.changeDirection('right')
-                if event.key == pygame.K_UP:
-                    p2.changeDirection('up')
-                if event.key == pygame.K_DOWN:
-                    p2.changeDirection('down')
-                if event.key == pygame.K_SPACE:
-                    p1creating = True
-                if event.key == pygame.K_w:
-                    p1.changeDirection('up')
-                if event.key == pygame.K_s:
-                    p1.changeDirection('down')
-                if event.key == pygame.K_a:
-                    p1.changeDirection('left')
-                if event.key == pygame.K_d:
-                    p1.changeDirection('right')
-                if event.key == pygame.K_RETURN:
-                    p2creating = True
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    p1creating = False
-                if event.key == pygame.K_RETURN:
-                    p2creating = False
+            if not gameScreen:
+                bgMusic.stop()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        done = True
+                    if event.key == pygame.K_LEFT:
+                        p2.changeDirection('left')
+                    if event.key == pygame.K_RIGHT:
+                        p2.changeDirection('right')
+                    if event.key == pygame.K_UP:
+                        p2.changeDirection('up')
+                    if event.key == pygame.K_DOWN:
+                        p2.changeDirection('down')
+                    if event.key == pygame.K_SPACE:
+                        p1creating = True
+                    if event.key == pygame.K_w:
+                        p1.changeDirection('up')
+                    if event.key == pygame.K_s:
+                        p1.changeDirection('down')
+                    if event.key == pygame.K_a:
+                        p1.changeDirection('left')
+                    if event.key == pygame.K_d:
+                        p1.changeDirection('right')
+                    if event.key == pygame.K_RETURN:
+                        p2creating = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE:
+                        p1creating = False
+                    if event.key == pygame.K_RETURN:
+                        p2creating = False
 
-        if p1creating:
-            p1.createTrail()
-        if p2creating:
-            p2.createTrail()
+        if not gameScreen:
+            if p1creating:
+                p1.createTrail()
+            if p2creating:
+                p2.createTrail()
 
-        for i in range(0, len(p1.trails)):
-            if p2.checkOutOfBounds() or p2.rect.colliderect(p1.trails[i]):
-                #Gameover('p1', gameover)
-                sb.addScore('p1')
-                restart(p1,p2)
-                break
-            if i - (len(p1.trails) - 5) < 0 and p1.rect.colliderect(p1.trails[i]):
-                #Gameover('p2', gameover)
-                sb.addScore('p2')
-                restart(p1,p2)
-                break
-        for i in range(0, len(p2.trails)):
-            if p1.checkOutOfBounds() or p1.rect.colliderect(p2.trails[i]):
-                #Gameover('p2', gameover)
-                sb.addScore('p2')
-                restart(p1,p2)
-                break
-            if i - (len(p2.trails) - 5) < 0 and p2.rect.colliderect(p2.trails[i]):
-                #Gameover('p1', gameover)
-                sb.addScore('p1')
-                restart(p1,p2)
-                break
+            for i in range(0, len(p1.trails)):
+                if p2.checkOutOfBounds() or p2.rect.colliderect(p1.trails[i]):
+                    # Gameover('p1', gameover)
+                    sb.addScore('p1')
+                    restart(p1, p2)
+                    break
+                if i - (len(p1.trails) - 5) < 0 and p1.rect.colliderect(p1.trails[i]):
+                    # Gameover('p2', gameover)
+                    sb.addScore('p2')
+                    restart(p1, p2)
+                    break
+            for i in range(0, len(p2.trails)):
+                if p1.checkOutOfBounds() or p1.rect.colliderect(p2.trails[i]):
+                    # Gameover('p2', gameover)
+                    sb.addScore('p2')
+                    restart(p1, p2)
+                    break
+                if i - (len(p2.trails) - 5) < 0 and p2.rect.colliderect(p2.trails[i]):
+                    # Gameover('p1', gameover)
+                    sb.addScore('p1')
+                    restart(p1, p2)
+                    break
 
-        if p1.rect.colliderect(p2.rect):
-            #Gameover('tie', gameover)
-            restart(p1,p2)
+            if p1.rect.colliderect(p2.rect):
+                # Gameover('tie', gameover)
+                restart(p1, p2)
 
-        redrawScreen(screen, display)
+            redrawScreen(screen, display)
+        else:
+            gameScreenLabels.clear(screen, display)
+            gameScreenLabels.update()
+            gameScreenLabels.draw(screen)
+            pygame.display.update()
 
     pygame.quit()
     quit()
